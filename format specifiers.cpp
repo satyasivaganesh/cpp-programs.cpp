8565b73ea5b127be89db3e24f3115fbc073bf67a
satyasivaganesh <84192998+satyasivaganesh@users.noreply.github.com>
2021-10-27 06:39:05 +0530

    Create format specifiers.cpp

---
+++ format specifiers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int i;long l;char c;float f;double d;
+    cin>>i>>l>>c>>f>>d;
+    cout<<i<<"\n"<<l<<"\n"<<c;
+    printf("\n%.3f\n%.9lf",f,d);
+    
+    
+    
